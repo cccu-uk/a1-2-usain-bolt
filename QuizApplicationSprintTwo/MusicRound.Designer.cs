c32b17e494527638ff479df9938579379114d831
LukeK2021 <89875022+LukeK2021@users.noreply.github.com>
2022-11-01 16:44:39 +0000

    asd

--- QuizApplicationSprintTwo/MusicRound.Designer.cs
+++ QuizApplicationSprintTwo/MusicRound.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.beginMusic = new QuizApplicationSprintTwo.MyButton();
+            this.myButton1 = new QuizApplicationSprintTwo.MyButton();
+            this.myButton2 = new QuizApplicationSprintTwo.MyButton();
+            this.myButton3 = new QuizApplicationSprintTwo.MyButton();
             this.SuspendLayout();
             // 
             // beginMusic
@@ -37,12 +40,51 @@
             this.beginMusic.FlatAppearance.BorderSize = 0;
             this.beginMusic.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.beginMusic.ForeColor = System.Drawing.Color.White;
-            this.beginMusic.Location = new System.Drawing.Point(299, 285);
+            this.beginMusic.Location = new System.Drawing.Point(114, 257);
             this.beginMusic.Name = "beginMusic";
             this.beginMusic.Size = new System.Drawing.Size(150, 40);
             this.beginMusic.TabIndex = 0;
-            this.beginMusic.Text = "Start!";
+            this.beginMusic.Text = "Answer one";
             this.beginMusic.UseVisualStyleBackColor = false;
+            // 
+            // myButton1
+            // 
+            this.myButton1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.myButton1.FlatAppearance.BorderSize = 0;
+            this.myButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.myButton1.ForeColor = System.Drawing.Color.White;
+            this.myButton1.Location = new System.Drawing.Point(484, 257);
+            this.myButton1.Name = "myButton1";
+            this.myButton1.Size = new System.Drawing.Size(150, 40);
+            this.myButton1.TabIndex = 1;
+            this.myButton1.Text = "myButton1";
+            this.myButton1.UseVisualStyleBackColor = false;
+            // 
+            // myButton2
+            // 
+            this.myButton2.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.myButton2.FlatAppearance.BorderSize = 0;
+            this.myButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.myButton2.ForeColor = System.Drawing.Color.White;
+            this.myButton2.Location = new System.Drawing.Point(484, 333);
+            this.myButton2.Name = "myButton2";
+            this.myButton2.Size = new System.Drawing.Size(150, 40);
+            this.myButton2.TabIndex = 2;
+            this.myButton2.Text = "myButton2";
+            this.myButton2.UseVisualStyleBackColor = false;
+            // 
+            // myButton3
+            // 
+            this.myButton3.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.myButton3.FlatAppearance.BorderSize = 0;
+            this.myButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.myButton3.ForeColor = System.Drawing.Color.White;
+            this.myButton3.Location = new System.Drawing.Point(114, 333);
+            this.myButton3.Name = "myButton3";
+            this.myButton3.Size = new System.Drawing.Size(150, 40);
+            this.myButton3.TabIndex = 3;
+            this.myButton3.Text = "myButton3";
+            this.myButton3.UseVisualStyleBackColor = false;
             // 
             // MusicRound
             // 
@@ -50,6 +92,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Yellow;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.myButton3);
+            this.Controls.Add(this.myButton2);
+            this.Controls.Add(this.myButton1);
             this.Controls.Add(this.beginMusic);
             this.ForeColor = System.Drawing.SystemColors.Control;
             this.Name = "MusicRound";
@@ -61,5 +106,8 @@
         #endregion
 
         private MyButton beginMusic;
+        private MyButton myButton1;
+        private MyButton myButton2;
+        private MyButton myButton3;
     }
 }
